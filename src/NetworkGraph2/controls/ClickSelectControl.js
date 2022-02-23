f5860267e5c6d23a2a03dbe192c9cbdb69ab8369
liaowei <liaowei@cestc.cn>
2022-02-23 10:35:08 +0800

    update

--- src/NetworkGraph2/controls/ClickSelectControl.js
+++ src/NetworkGraph2/controls/ClickSelectControl.js
@@ -5,40 +5,41 @@ export default class ClickSelectControl extends EventEmitter {
     constructor(graph) {
         super();
 
-        const handleClickNode = function (e, node) {
+        const handleClickElement = function (e, elem) {
             e.stopPropagation();
-            graph.model.eachNode(child => {
-                const hit = child.data.id === node.data.id;
+
+            let hitElem;
+            graph.model.traverse(child => {
+                const hit = child.data.id === elem.data.id;
                 child.data.selected = hit;
                 if (hit) {
+                    hitElem = child;
                     child.data.color = '#99f';
-                    child.data.size = 20;
-                    graph.model.emit('selectNode', { type: 'selectNode', target: child });
+                    if (child.type === 'node') {
+                        child.data.size = 20;
+                    } else if (child.type === 'edge') {
+                        //
+                    }
                 } else {
                     child.data.color = '#000';
-                    child.data.size = 15;
+                    if (child.type === 'node') {
+                        child.data.size = 15;
+                    }
                 }
-            });
-            graph.renderer.render(graph.model);
+            })
+
+            if (hitElem) {
+                if (hitElem.type === 'node') {
+                    graph.model.emit('selectNode', { type: 'selectNode', target: hitElem });
+                } else if (hitElem.type === 'edge') {
+                    graph.model.emit('selectEdge', { type: 'selectEdge', target: hitElem });
+                }
+                graph.render();
+            }
         };
 
-        const handleClickEdge = function (e, edge) {
-            e.stopPropagation();
-            graph.model.eachEdge(child => {
-                const hit = child.data.id === edge.data.id;
-                child.data.selected = hit;
-                if (hit) {
-                    child.data.color = '#99f';
-                    graph.model.emit('selectEdge', { type: 'selectEdge', target: child });
-                } else {
-                    child.data.color = '#000';
-                }
-            });
-            graph.renderer.render(graph.model);
-        };
-
-        graph.renderer.on('createdNode', enter => enter.on('click', handleClickNode));
-        graph.renderer.on('createdEdge', enter => enter.on('click', handleClickEdge));
+        graph.renderer.on('createdNode', enter => enter.on('click', handleClickElement));
+        graph.renderer.on('createdEdge', enter => enter.on('click', handleClickElement));
         graph.renderer.rootSelection.on('click', (e) => {
             graph.model.traverse(child => {
                 child.data.selected = false;
