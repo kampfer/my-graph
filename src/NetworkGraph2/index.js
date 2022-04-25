11c3d6eccc4bd27554698e164b22840538587a70
liaowei <liaowei@cestc.cn>
2022-04-25 14:37:07 +0800

    删除react renderer

--- src/NetworkGraph2/index.js
+++ src/NetworkGraph2/index.js
@@ -1,5 +1,4 @@
 export { default as D3Renderer } from './renderers/d3/D3Renderer.js';
-export { default as ReactRenderer } from './renderers/react/ReactRenderer.js';
 export { default as Graph } from './objects/Graph.js';
 export { default as Node } from './objects/Node.js';
 export { default as Edge } from './objects/Edge.js';
@@ -7,13 +6,7 @@ export { default as ForceLayout } from './layouts/ForceLayout.js';
 export { default as ZoomControl } from './controls/ZoomControl.js';
 export { default as DragControl } from './controls/DragControl.js';
 
-// export { default as ReactNode } from './views/react/Node.js';
-// export { default as ReactEdge } from './views/react/Edge.js';
-
 export { default as D3Node } from './views/d3/Node.js';
 export { default as D3Edge } from './views/d3/Edge.js';
-
-// export { default as ReactDragControl } from './controls/react/DragControl.js';
-// export { default as DraggableComponent } from './views/react/draggableComponent.js';
 
 export { default as NetworkGraph } from './NetworkGraph';
